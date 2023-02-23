799827d589dc7f0c3493287038813335b950c7ae
akbar19910918 <akbarachilov91@gmail.com>
2023-02-23 14:51:31 +0500

    test

---
+++ index.js
@@ -0,0 +1,15 @@
+//String =="",'',``
+//Number  ==Number,NaN,Infinity
+//undefined==bosh joy,
+//null==bosh joy,
+//boolen===true,false
+//BigInt==katta son
+//Symbol=kalit soz.
+
+//Object=malumotlar ombori
+
+//var,let,const.
+//array,obj,function
+//array[]
+//obj{}
+//function () {}
